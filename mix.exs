45bdc69a066dbba0afe041f96a1ecd27682b4f67
camshaft <bytheway.cameron@gmail.com>
2015-04-22 16:24:54 -0600

    release 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Etude.Mixfile do
 
   def project do
     [app: :etude,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.0",
      description: "parallel computation coordination compiler for erlang/elixir",
      deps: deps,
