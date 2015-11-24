72b9b363aae42bdef85c3c42f98e41c90c57b1d7
Leopoldo Godines <lg@leopoldogodines.com>
2015-11-24 21:05:43 -0200

    Almost there

--- lib/etude/node/partial.ex
+++ lib/etude/node/partial.ex
@@ -17,27 +17,36 @@ defmodule Etude.Node.Partial do
     defdelegate var(node, opts), to: Etude.Node.Any
 
     def children(node) do
-      :maps.to_list(node.props)
+      IO.inspect [props: :maps.to_list(node.props), function: node.function, module: node.module, where: :children]
+      [props: :maps.to_list(node.props), function: node.function, module: node.module]
     end
 
-    def set_children(node, props) do
-      %{node | props: :maps.from_list(props)}
+    def set_children(node, [props: props, function: function, module: module]) do
+      IO.inspect [props: props, function: function, module: module, where: :set_children]
+      %{node | props: :maps.from_list(props), function: function, module: node.module}
     end
 
     def compile(node, opts) do
-      mod = escape(node.module)
-      fun_e = escape(node.function)
-      fun_a = "#{fun_e}_partial" |> String.to_atom
-      fun = escape(fun_a)
-      props = node.props
-      scope = Etude.Runtime.hash({mod, fun, props})
+      opts = opts ++ [debug: true]
 
-      defop node, opts, [:memoize], """
-      _Props = #{Children.props(props, opts)},
-      #{child_scope(scope)},
-      #{debug_call(node.module, fun_a, "[_Props]", opts)},
-      #{mod}:#{fun}(#{op_args}, _Props)
-      """, Children.compile(Map.values(props), opts)
+      x = """
+      _Props = #{Children.props(node.props, opts)},
+      #{Children.call([node.module, node.function], opts)},
+      
+      case {#{Children.vars([node.module, node.function], opts)}} of
+        {{#{ready}, Mod}, {#{ready}, Func}} ->
+          io:put_chars([<<"DEBUG | ">>, <<" #{opts[:name]} :: ">>, [<<"calling ">>, atom_to_binary(Mod, utf8), <<".">>, atom_to_binary(Func, utf8), <<"(">>, 'Elixir.Enum':join('Elixir.Enum':map([_Props], fun etude_inspect/1), <<", ">>), <<")">>], <<"\n">>]),
+          rebind(#{scope}) = {'Elixir.Etude.Runtime':hash({#{scope}, #{Children.vars([node.function], opts)}, #{Children.vars([node.module], opts)}}), 0},
+          apply(Mod, Func, [#{op_args}, _Props]);
+        #{Children.wildcard([node.function], opts)} ->
+          io:put_chars([<<"DEBUG | ">>, <<" #{opts[:name]} :: ">>, [<<"waiting for function name for partial with props: ">>, 'Elixir.Enum':join('Elixir.Enum':map([_Props], fun etude_inspect/1), <<", ">>)], <<"\n">>]),
+          {nil, #{state}}
+      end
+      """
+      IO.puts "--------------"
+      IO.puts x
+      IO.puts "--------------"
+      defop node, opts, [:memoize], x, Children.compile(Map.values(node.props) ++ [node.function, node.module], opts)
     end
   end
 end
