731313aa1da8b98053d007942487467c3441643e
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-04-26 02:30:22 -0600

    release 1.0.0-beta.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Etude.Mixfile do
 
   def project do
     [app: :etude,
-     version: "1.0.0-beta.0",
+     version: "1.0.0-beta.1",
      elixir: "~> 1.0",
      description: "parallel computation coordination utilities for erlang/elixir",
      deps: deps,
